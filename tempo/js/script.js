b4ba0d97ebaef02fa53a4088f39ade08d20e0dd0
luizpireskotcho <99926993+luizpireskotcho@users.noreply.github.com>
2025-11-26 19:12:15 +0000

    primeira

---
+++ tempo/js/script.js
@@ -0,0 +1,14 @@
+//DOM
+const cidade = document.querySelector('#cidade')
+const bprocurar = document.querySelector('#bprocurar')
+const city = document.querySelector('#city')
+const temperatura = document.querySelector('#temperatura')
+const previsao = document.querySelector('#previsao')
+const chave = '84a23f5063fa12a586d40f6e347d76d3'
+
+//Evento
+
+bprocurar.addEventListener('click',buscarcidade)
+
+//Função
+
